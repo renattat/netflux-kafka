27805a4f0407e3c0a38ec28a70f51be3c24112aa
Renat Sim <renat.tat@mail.ru>
2026-09-07 13:45:46 +0300

    send genre update event in customer service via application event publisher

--- customer-service/src/main/java/com/renat/customer/messaging/CustomerEventPublisher.java
+++ customer-service/src/main/java/com/renat/customer/messaging/CustomerEventPublisher.java
@@ -1,14 +1,31 @@
 package com.renat.customer.messaging;
 
 import com.renat.netflux.events.CustomerGenreUpdatedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.context.event.EventListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerEventPublisher {
+    private static final Logger log = LoggerFactory.getLogger(CustomerEventPublisher.class);
+    private static final String CUSTOMER_EVENT_OUT = "customer-events-out";
+    private final StreamBridge streamBridge;
+
+    public CustomerEventPublisher(StreamBridge streamBridge) {
+        this.streamBridge = streamBridge;
+    }
 
     @EventListener
     public void onGenreUpdatedEvent(CustomerGenreUpdatedEvent genreUpdatedEvent) {
+        var message = MessageBuilder.withPayload(genreUpdatedEvent)
+                .setHeader(KafkaHeaders.KEY, genreUpdatedEvent.customerId())
+                .build();
+        this.streamBridge.send(CUSTOMER_EVENT_OUT, message);
+        log.info("published: {}", message);
 
     }
 
--- customer-service/src/main/java/com/renat/customer/service/CustomerService.java
+++ customer-service/src/main/java/com/renat/customer/service/CustomerService.java
@@ -2,17 +2,36 @@ package com.renat.customer.service;
 
 import com.renat.customer.dto.CustomerDetails;
 import com.renat.customer.dto.GenreUpdateRequest;
+import com.renat.customer.exception.CustomerNotFoundException;
+import com.renat.customer.mapper.CustomerMapper;
+import com.renat.customer.repository.CustomerRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
 
-    public CustomerDetails getCustomer(Integer customerId) {
-        return null;
+    private final CustomerRepository customerRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    public CustomerService(CustomerRepository customerRepository, ApplicationEventPublisher eventPublisher) {
+        this.customerRepository = customerRepository;
+        this.eventPublisher = eventPublisher;
     }
 
-    public void updateCustomerGenre(Integer customerId, GenreUpdateRequest request) {
+    public CustomerDetails getCustomer(Integer customerId) {
+        return this.customerRepository.findById(customerId)
+                .map(CustomerMapper::toCustomerDetails)
+                .orElseThrow(()-> new CustomerNotFoundException(customerId));
+    }
 
+    @Transactional
+    public void updateCustomerGenre(Integer customerId, GenreUpdateRequest request) {
+        var customer = this.customerRepository.findById(customerId)
+                .orElseThrow(()-> new CustomerNotFoundException(customerId));
+        customer.setFavoriteGenre(request.favoriteGenre());
+        this.eventPublisher.publishEvent(CustomerMapper.toGenreUpdatedEvent(customer.getId(), request.favoriteGenre()));
     }
 
 }

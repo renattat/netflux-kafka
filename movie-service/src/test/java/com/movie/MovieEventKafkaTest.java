27455ce1355feaf687d19a9c403c7abe86319e0f
Renat Sim <renat.tat@mail.ru>
2026-09-10 13:29:09 +0300

    Movie service: testcontainers integration test

--- movie-service/src/test/java/com/movie/MovieEventKafkaTest.java
+++ movie-service/src/test/java/com/movie/MovieEventKafkaTest.java
@@ -1,6 +1,8 @@
 package com.movie;
 
+import com.movie.dto.MovieDetails;
 import com.netflux.events.MovieAddedEvent;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
@@ -8,11 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.test.web.servlet.client.RestTestClient;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 @AutoConfigureRestTestClient
@@ -38,8 +43,32 @@ public class MovieEventKafkaTest {
     private BlockingQueue<Message<MovieAddedEvent>> queue;
 
     @Test
-    public void movieAddedEvent() {
+    public void movieAddedEvent() throws InterruptedException {
+        // send the post request
+        var json = """
+                {"title":"The Fifth Element","voteAverage":7.53,"voteCount":9947,"releaseDate":"1997-05-02","revenue":263920180,"runtime":126,"backdropPath":"/wgvc3PmjQGtYYDWaeuV867mnFDs.jpg","budget":90000000,"homepage":"","overview":"In 2257, a taxi driver is unintentionally given the task of saving a young girl who is part of the key that will ensure the survival of humanity.","popularity":49.67,"posterPath":"/fPtlCO1yQtnoLHOwKtWz7db6RGU.jpg","genres":["Adventure","Fantasy","Action","Thriller","Science Fiction"]}
+                """;
+        var request = JsonMapper.shared().readValue(json, MovieDetails.class);
+        var response = this.testClient.post()
+                .uri("/api/movies")
+                .body(request)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .returnResult(MovieDetails.class)
+                .getResponseBody();
 
+        // validate the movie response
+        Assertions.assertNotNull(response);
+        Assertions.assertNotNull(response.id());
+        Assertions.assertEquals("The Fifth Element", response.title());
+
+        // validate movie added event
+        var message = this.queue.poll(5, TimeUnit.SECONDS);
+        Assertions.assertNotNull(message);
+        var event = message.getPayload();
+        Assertions.assertEquals(response.id(), message.getHeaders().get(KafkaHeaders.RECEIVED_KEY, Integer.class));
+        Assertions.assertEquals(response.id(), event.movieId());
+        Assertions.assertEquals("The Fifth Element", event.title());
     }
 
     @TestConfiguration

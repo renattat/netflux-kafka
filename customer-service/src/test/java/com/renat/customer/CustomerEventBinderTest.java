bcbbe9fd79eedf2f505167322c4c2e8805637c1c
Renat Sim <renat.tat@mail.ru>
2026-09-10 10:03:08 +0300

    Customer service test binder integration test

--- customer-service/src/test/java/com/renat/customer/CustomerEventBinderTest.java
+++ customer-service/src/test/java/com/renat/customer/CustomerEventBinderTest.java
@@ -1,12 +1,17 @@
 package com.renat.customer;
 
+import com.netflux.events.CustomerGenreUpdatedEvent;
+import com.renat.customer.dto.GenreUpdateRequest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.stream.binder.test.EnableTestBinder;
 import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.test.web.servlet.client.RestTestClient;
+import tools.jackson.databind.json.JsonMapper;
 
 @SpringBootTest
 @EnableTestBinder
@@ -21,6 +26,18 @@ public class CustomerEventBinderTest {
 
     @Test
     public void genreUpdatedEvent() {
-    }
+        var request = new GenreUpdateRequest("Thriller");
+        this.testClient.patch()
+                .uri("/api/customers/2/genre")
+                .body(request)
+                .exchange().expectStatus().isNoContent();
 
+        var message = this.outputDestination.receive(1000,  "customer-events");
+        var event = JsonMapper.shared().readValue(message.getPayload(), CustomerGenreUpdatedEvent.class);
+
+        // validate the event
+        Assertions.assertEquals(2, message.getHeaders().get(KafkaHeaders.KEY, Integer.class));
+        Assertions.assertEquals(2, event.customerId());
+        Assertions.assertEquals("Thriller", event.favoriteGenre());
+    }
 }

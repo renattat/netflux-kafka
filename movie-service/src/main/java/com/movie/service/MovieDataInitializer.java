6ecc12509bac7cec94d637a2a8ff17761a9ebee5
Renat Sim <renat.tat@mail.ru>
2026-09-10 12:46:03 +0300

    Movie service implementation

--- movie-service/src/main/java/com/movie/service/MovieDataInitializer.java
+++ movie-service/src/main/java/com/movie/service/MovieDataInitializer.java
@@ -1,16 +1,54 @@
 package com.movie.service;
 
+import com.movie.dto.MovieDetails;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import tools.jackson.databind.json.JsonMapper;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Service // import the movies for demo purposes
 @ConditionalOnProperty(name = "app.import-movies", havingValue = "true")
 public class MovieDataInitializer implements CommandLineRunner {
 
+    private final MovieService movieService;
+    private final Resource resource;
+
+    public MovieDataInitializer(MovieService movieService, @Value("classpath:movies.jsonl") Resource resource) {
+        this.movieService = movieService;
+        this.resource = resource;
+    }
+
     @Override
     public void run(String... args) throws Exception {
+        Thread.ofVirtual().start(this::loadMovies);
+    }
 
+    private void loadMovies() {
+        var mapper = JsonMapper.shared();
+        try (var reader = new BufferedReader(new InputStreamReader(resource.getInputStream()))) {
+            reader.lines()
+                    .map((String line) -> mapper.readValue(line, MovieDetails.class))
+                    .forEach((MovieDetails movieDetails) -> {
+                        this.movieService.saveMovie(movieDetails);
+                        this.sleep(3000);
+                    });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- movie-service/src/main/java/com/movie/service/MovieService.java
+++ movie-service/src/main/java/com/movie/service/MovieService.java
@@ -2,18 +2,35 @@ package com.movie.service;
 
 
 import com.movie.dto.MovieDetails;
+import com.movie.exception.MovieNotFoundException;
+import com.movie.mapper.MovieMapper;
+import com.movie.repository.MovieRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MovieService {
 
-    public MovieDetails getMovie(Integer movieId) {
-        return null;
+    private final MovieRepository movieRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    public MovieService(MovieRepository movieRepository, ApplicationEventPublisher eventPublisher) {
+        this.movieRepository = movieRepository;
+        this.eventPublisher = eventPublisher;
     }
 
+    public MovieDetails getMovie(Integer movieId) {
+        return this.movieRepository.findById(movieId)
+                .map(MovieMapper::toMovieDetails)
+                .orElseThrow(() -> new MovieNotFoundException(movieId));
+    }
 
+    @Transactional
     public MovieDetails saveMovie(MovieDetails movieDetails) {
-        return null;
+        var movie = this.movieRepository.save(MovieMapper.toMovie(movieDetails));
+        this.eventPublisher.publishEvent(MovieMapper.toMovieAddedEvent(movie));
+        return MovieMapper.toMovieDetails(movie);
     }
 
 }
